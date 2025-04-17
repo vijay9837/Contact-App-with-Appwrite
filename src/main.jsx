2f21327b996e9f40c7a0c90f25876744c2dbc7b5
vijay9837 <vkparjapati555@gmail.com>
2025-04-17 15:27:07 +0530

    changes at main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './Context.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HashRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
+    </HashRouter>
   </StrictMode>,
 )
